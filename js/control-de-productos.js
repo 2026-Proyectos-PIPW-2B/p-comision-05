8396e591c91d5d0c1d0a1cd022a88b310c0a8266
Joaquin <joacopompilio@gmail.com>
2026-06-24 14:46:48 -0300

    Se modifico todo control-de-productos, se corrigio bug de modales y se implemento una verificacion para que no pueda haber 2 productos iguales

--- js/control-de-productos.js
+++ js/control-de-productos.js
@@ -1,4 +1,5 @@
 import * as moduloProductos from "../modulos/stock.js";
+import { crearFilaProducto } from "../modulos/crearDomElements.js";
 let funcionParaBotonEditar = null;
 
 //para probar la tabla
@@ -32,308 +33,248 @@ if (!localStorage.getItem("stock")) {
 }
 
 const tbody = document.getElementById("cuerpoDeTabla");
-const botonResetearFiltros = document.getElementById("botonResetearFiltros");
+const formRegistro = document.getElementById("formRegistroProductos");
+const botonGuardar = document.getElementById("botonRegistroGuardar");
+const botonGuardarCambios = document.getElementById("botonGuardarCambios");
+
+//elementos de los filtros
 const filtroTitulo = document.getElementById("filtroTitulo");
 const filtroDescripcion = document.getElementById("filtroDescripcion");
 const filtroStock = document.getElementById("filtroStock");
 const filtroEtiqueta = document.getElementById("filtroEtiqueta");
 const botonAplicarFiltros = document.getElementById("botonAplicarFiltros");
+const botonResetearFiltros = document.getElementById("botonResetearFiltros");
 
-const misProductos = moduloProductos.getStock();
-
-const filtros = {
+let idProductoEnEdicion = null;
+const filtrosActivos = {
   titulo: "",
   descripcion: "",
   cantidad: "",
   etiqueta: "",
 };
+//listeners
+botonGuardar.addEventListener("click", registrarNuevoProducto);
+botonGuardarCambios.addEventListener("click", guardarCambiosProducto);
+botonAplicarFiltros.addEventListener("click", aplicarFiltros);
+botonResetearFiltros.addEventListener("click", resetearFiltros);
 
-botonResetearFiltros.addEventListener("click", function () {
-  filtros.titulo = "";
-  filtros.descripcion = "";
-  filtros.cantidad = "";
-  filtros.etiqueta = "";
 
+//funciones 
+function actualizarTablaCompleta() {
+  tbody.innerHTML = "";
+  const misProductos = moduloProductos.getStock();
+  let contador = 1;
+
+  const filtroTitulo = filtrosActivos.titulo.toUpperCase().trim();
+  const filtroDescripcion = filtrosActivos.descripcion.toUpperCase().trim();
+  let filtroCantidad;
+  if (filtrosActivos.cantidad) {
+    filtroCantidad = parseInt(filtrosActivos.cantidad);
+  } else {
+    filtroCantidad = null;
+  }
+  const filtroEtiqueta = filtrosActivos.etiqueta.toUpperCase().trim();
+
+  for (const [id, producto] of misProductos) {
+    // Validacion de Filtros
+    if (filtroTitulo && !producto.nombre.toUpperCase().includes(filtroTitulo))
+      continue;
+    if (
+      filtroDescripcion &&
+      !producto.descripcion.toUpperCase().includes(filtroDescripcion)
+    )
+      continue;
+    if (filtroCantidad !== null && producto.cantidad !== filtroCantidad)
+      continue;
+    if (filtroEtiqueta) {
+      let tieneEtiqueta = false;
+      for (let i = 0; i < producto.etiquetas.length; i++) {
+        if (producto.etiquetas[i].toUpperCase() === filtroEtiqueta) {
+          tieneEtiqueta = true;
+          break;
+        }
+      }
+      if (!tieneEtiqueta) continue;
+    }
+
+    const fila = crearFilaProducto(
+      producto,
+      contador,
+      eliminarProducto,
+      iniciarEdicionProducto,
+    );
+    tbody.appendChild(fila);
+    contador++;
+  }
+}
+
+function registrarNuevoProducto() {
+  const titulo = document.getElementById("registroTitulo").value.trim();
+  const info = document.getElementById("registroInfo").value.trim();
+  const stock = parseInt(document.getElementById("registroStock").value);
+  const valor = parseFloat(document.getElementById("registroValor").value);
+  const registroEtiquetas = document
+    .getElementById("registroEtiquetas")
+    .value.trim();
+
+  if (!titulo || !info || isNaN(stock) || isNaN(valor)) {
+    alert("Por favor completar los campos faltantes");
+    return;
+  }
+
+  const misProductos = moduloProductos.getStock(); // Traemos el Map de productos
+  let nombreDuplicado = false;
+
+  for (const [id, producto] of misProductos) {
+    if (producto.nombre.toUpperCase().trim() === titulo.toUpperCase()) {
+      nombreDuplicado = true;
+      break; 
+    }
+  }
+
+  if (nombreDuplicado) {
+    alert("ya existe un producto registrado con ese nombre, elegir otro.");
+    return;
+  }
+
+  let etiquetasArray = [];
+  if (registroEtiquetas) {
+    const arraySeparado = registroEtiquetas.split(",");
+    for (let i = 0; i < arraySeparado.length; i++) {
+      etiquetasArray.push(arraySeparado[i].trim());
+    }
+  }
+
+  moduloProductos.setProducto(titulo, etiquetasArray, info, stock, valor);
+  const todosLosProductos = moduloProductos.getStock();
+  const llaves = Array.from(todosLosProductos.keys());
+  const ultimoId = llaves[llaves.length - 1];
+  const productoCreado = moduloProductos.getProducto(ultimoId);
+
+  const proximoIndice = tbody.children.length + 1;
+
+  if (productoCreado) {
+    const nuevaFila = crearFilaProducto(productoCreado,proximoIndice,eliminarProducto,iniciarEdicionProducto,);
+    tbody.appendChild(nuevaFila);
+  }
+
+  formRegistro.reset();
+  //busco si hay un modal activo
+  const modalInstancia = bootstrap.Modal.getInstance(
+    document.getElementById("modal"),
+  );
+  //si el modal existe, lo oculto
+  if (modalInstancia) modalInstancia.hide();
+}
+
+function iniciarEdicionProducto(id) {
+  idProductoEnEdicion = id;
+  const producto = moduloProductos.getProducto(id);
+
+  if (producto) {
+    document.getElementById("editar-titulo").value = producto.nombre;
+    document.getElementById("editar-info").value = producto.descripcion;
+    document.getElementById("editar-stock").value = producto.cantidad;
+    document.getElementById("editar-valor").value = producto.valor;
+
+    const divEtiquetas = document.getElementById("editar-etiquetas");
+    let inputEtiquetas = divEtiquetas.querySelector("input.inputEtiquetas");
+    if (!inputEtiquetas) {
+      inputEtiquetas = document.createElement("input");
+      inputEtiquetas.classList.add("form-control", "inputEtiquetas", "mt-2");
+      divEtiquetas.appendChild(inputEtiquetas);
+    }
+    if (producto.etiquetas) {
+      //con join se unen todos elementos de un array a string, con una "," y un espacio
+      inputEtiquetas.value = producto.etiquetas.join(", ");
+    } else {
+      inputEtiquetas.value = "";
+    }
+  }
+}
+
+function guardarCambiosProducto() {
+  //chequeo que haya un producto en edicion (que idProductoEnEdicion no sea null)
+  if (!idProductoEnEdicion) return;
+
+  const tituloNuevo = document.getElementById("editar-titulo").value.trim();
+  const informacionNueva = document.getElementById("editar-info").value.trim();
+  const stockNuevo = parseInt(document.getElementById("editar-stock").value);
+  const nuevoValor = parseFloat(document.getElementById("editar-valor").value);
+  const inputEtiquetas = document
+    .getElementById("editar-etiquetas")
+    .querySelector(".inputEtiquetas");
+  let etiquetasNuevas = [];
+  if (inputEtiquetas && inputEtiquetas.value) {
+    const arraySeparado = inputEtiquetas.value.split(",");
+    for (let i = 0; i < arraySeparado.length; i++) {
+      etiquetasNuevas.push(arraySeparado[i].trim()); //tambien elimino los espacios de la etiqueatas
+    }
+  }
+
+  moduloProductos.editarProducto(
+    idProductoEnEdicion,
+    tituloNuevo,
+    informacionNueva,
+    stockNuevo,
+    nuevoValor,
+  );
+  const producto = moduloProductos.getProducto(idProductoEnEdicion);
+  if (producto) producto.etiquetas = etiquetasNuevas;
+
+  moduloProductos.saveStock();
+
+  //reemplazo directo de la fila en el HTML
+  const filaVieja = tbody.querySelector(`[data-id="${idProductoEnEdicion}"]`);
+  if (filaVieja && producto) {
+    const numeroActual = filaVieja.querySelector("td").textContent;
+
+    const filaNueva = crearFilaProducto(
+      producto,
+      numeroActual,
+      eliminarProducto,
+      iniciarEdicionProducto,
+    );
+    tbody.replaceChild(filaNueva, filaVieja);
+  }
+
+  //chekeo si el modal esta activo, si esta lo oculto y borro idProductoEnEdicion
+  const modalInstancia = bootstrap.Modal.getInstance(
+    document.getElementById("modalEditar"),
+  );
+  if (modalInstancia) {
+  modalInstancia.hide();
+  idProductoEnEdicion = null;
+  }
+}
+
+function eliminarProducto(id) {
+  moduloProductos.removeProducto(id);
+
+  const filaAEliminar = tbody.querySelector(`[data-id="${id}"]`);
+  if (filaAEliminar) {
+    filaAEliminar.remove();
+  }
+}
+
+function aplicarFiltros() {
+  filtrosActivos.titulo = filtroTitulo.value;
+  filtrosActivos.descripcion = filtroDescripcion.value;
+  filtrosActivos.cantidad = filtroStock.value;
+  filtrosActivos.etiqueta = filtroEtiqueta.value;
+  actualizarTablaCompleta();
+}
+
+function resetearFiltros() {
+  filtrosActivos.titulo = "";
+  filtrosActivos.descripcion = "";
+  filtrosActivos.cantidad = "";
+  filtrosActivos.etiqueta = "";
   filtroTitulo.value = "";
-  filtroDescripcion.value = "";
   filtroDescripcion.value = "";
   filtroStock.value = "";
   filtroEtiqueta.value = "";
-
-  mostrarProductos();
-});
-
-botonAplicarFiltros.addEventListener("click", function () {
-  console.log("estoy en el boton de filtros");
-  filtros.titulo = filtroTitulo.value;
-  filtros.descripcion = filtroDescripcion.value;
-  filtros.cantidad = filtroStock.value;
-  filtros.etiqueta = filtroEtiqueta.value;
-  mostrarProductos();
-});
-
-export function mostrarProductos() {
-  const misProductos = moduloProductos.getStock();
-  tbody.innerHTML = "";
-  let contador = 1;
-
-  //variables del filtro
-  let titulo = filtros.titulo.toUpperCase().trim();
-  let descripcion = filtros.descripcion.toUpperCase().trim();
-  let cantidad = filtros.cantidad;
-  let etiqueta = filtros.etiqueta.toUpperCase().trim();
-  let estadoDeFiltro = !(
-    titulo === "" &&
-    descripcion === "" &&
-    cantidad === "" &&
-    etiqueta === ""
-  );
-
-  for (const [id, producto] of misProductos) {
-    console.log("estoy viendo un producto");
-    console.log(producto);
-    //filtrado
-    if (estadoDeFiltro) {
-      if (titulo !== "" && !producto.nombre.toUpperCase().includes(titulo))
-        continue; //si estaba buscando por titulo y el titulo no coincide, no lo muestra
-
-      if (
-        descripcion !== "" &&
-        !producto.descripcion.toUpperCase().includes(descripcion)
-      )
-        continue;
-
-      if (cantidad !== "" && !(producto.cantidad === parseInt(cantidad)))
-        continue;
-
-      if (etiqueta !== "") {
-        let encontro = false;
-        const arrayEtiquetas = producto.etiquetas;
-        for (let i = 0; i < arrayEtiquetas.length && !encontro; i++) {
-          encontro = arrayEtiquetas[i].toUpperCase() === etiqueta;
-        }
-        if (!encontro) continue;
-      }
-    }
-
-    console.log(document.getElementById(id));
-    //creacion de tabla
-    const fila = document.createElement("tr");
-    fila.id = producto.id;
-    //#
-    let celda = document.createElement("td");
-    celda.textContent = contador;
-    fila.appendChild(celda);
-
-    //Titulo
-    let celdaTitulo = document.createElement("td");
-    celdaTitulo.textContent = producto.nombre;
-    fila.appendChild(celdaTitulo);
-
-    //Información
-    const celdaDescripcion = document.createElement("td");
-    celdaDescripcion.textContent = producto.descripcion;
-    fila.appendChild(celdaDescripcion);
-
-    //Stock
-    const celdaStock = document.createElement("td");
-    celdaStock.textContent = producto.cantidad;
-    fila.appendChild(celdaStock);
-
-    //Etiquetas
-
-    const celdaEtiquetas = document.createElement("td");
-    const etiquetas = producto.etiquetas;
-    if (etiquetas.length > 0) {
-      for (let i = 0; i < etiquetas.length; i++) {
-        const contenedorEtiqueta = document.createElement("div");
-        const etiqueta = document.createElement("span");
-        etiqueta.textContent = etiquetas[i];
-        //<i class="bi bi-backspace-reverse"></i>
-        const icono = document.createElement("i");
-        icono.classList.add("bi", "bi-backspace-reverse", "d-none");
-
-        icono.classList.add("btn", "btn-sm");
-        contenedorEtiqueta.classList.add("d-flex");
-        contenedorEtiqueta.classList.add(coloresRandon());
-        contenedorEtiqueta.classList.add("badge", "rounded-pill");
-
-        contenedorEtiqueta.appendChild(etiqueta);
-        etiqueta.appendChild(icono);
-        celdaEtiquetas.appendChild(contenedorEtiqueta);
-
-        icono.addEventListener("click", function (e) {
-          console.log("estoy en el boton");
-          //eliminar etiqueta
-          contenedorEtiqueta.classList.add("d-none");
-        });
-      }
-      fila.appendChild(celdaEtiquetas);
-
-      //Valor
-      const celdaValor = document.createElement("td");
-      celdaValor.textContent = "$" + producto.valor;
-      fila.appendChild(celdaValor);
-
-      //Acciones
-      const celdaBotones = document.createElement("td");
-      celdaBotones.appendChild(crearBotonEliminar(id));
-      celdaBotones.appendChild(crearBotonEditar(id));
-      fila.appendChild(celdaBotones);
-
-      tbody.appendChild(fila);
-      contador++;
-    }
-  }
-}
-function crearBotonEliminar(id) {
-  const botonEliminar = document.createElement("i");
-  botonEliminar.classList.add("bi", "bi-trash3-fill", "btn", "btn-sm");
-
-  botonEliminar.addEventListener("click", function () {
-    moduloProductos.removeProducto(id);
-    mostrarProductos();
-    //document.getElementById(id).remove() usar para eliminar elementos
-  });
-  return botonEliminar;
+  actualizarTablaCompleta();
 }
 
-function crearBotonEditar(id) {
-  document
-    .querySelectorAll(".etiquetasEditar .choclo")
-    .forEach(function (hijo) {
-      hijo.remove();
-    });
-  const botonEditar = document.createElement("i");
-  const botonGuardarCambios = document.getElementById("botonGuardarCambios");
-
-  botonEditar.classList.add("bi", "bi-pencil-square", "btn", "btn-sm");
-
-  // Hago que el boton me apunte al modal editar, de esta forma cuando toco el boton, se abre ese modal
-  botonEditar.setAttribute("data-bs-toggle", "modal");
-  botonEditar.setAttribute("data-bs-target", "#modalEditar");
-
-  botonEditar.addEventListener("click", function () {
-    let elementos = document.querySelectorAll(".choclo");
-    elementos.forEach((elemento) => {
-      elemento.remove();
-    });
-    document
-      .querySelectorAll(".etiquetasEditar .choclo")
-      .forEach(function (hijo) {
-        hijo.remove();
-      });
-      
-    const producto = moduloProductos.getProducto(id);
-
-    if (producto) {
-      document
-        .querySelectorAll(".etiquetasEditar .choclo")
-        .forEach(function (hijo) {
-          hijo.remove();
-        });
-      //asigno los value correspondientes al modal
-      document.getElementById("editar-titulo").value = producto.nombre;
-      document.getElementById("editar-info").value = producto.descripcion;
-      document.getElementById("editar-stock").value = producto.cantidad;
-      document.getElementById("editar-valor").value = producto.valor;
-      let etiquetas = document.getElementById("editar-etiquetas");
-      mostrarEtiquetas(etiquetas, producto.etiquetas);
-
-      funcionParaBotonEditar = function () {
-        document
-          .querySelectorAll(".etiquetasEditar .choclo")
-          .forEach(function (hijo) {
-            hijo.remove();
-          });
-        let idProducto = producto.id;
-        let tituloNuevo = document.getElementById("editar-titulo").value;
-        let informacionNueva = document.getElementById("editar-info").value;
-        let inputModalEtiquetas = document.getElementById("editar-etiquetas");
-        let stockNuevo = document.getElementById("editar-stock").value;
-        let nuevoValor = document.getElementById("editar-valor").value;
-
-        // Modifico el objeto original, con los elementos ingresados
-        moduloProductos.editarProducto(
-          idProducto,
-          tituloNuevo,
-          informacionNueva,
-          stockNuevo,
-          nuevoValor,
-        );
-        // guardo en LocalStorage, muestro la tabla y termino el evento del modal
-        moduloProductos.saveStock();
-        mostrarProductos();
-
-        // se cierra el modal de edición
-        document.querySelector("#modalEditar .btn-close").click();
-        document
-          .querySelectorAll(".etiquetasEditar .choclo")
-          .forEach(function (hijo) {
-            hijo.remove();
-          });
-      };
-      botonGuardarCambios.addEventListener("click", funcionParaBotonEditar);
-    }
-  });
-  return botonEditar;
-}
-
-//Agrego un listener al cierre del modal(funciona cuando se toca la cruz o hago click fuera del modal o con escape)
-document
-  .getElementById("modalEditar")
-  .addEventListener("hide.bs.modal", function () {
-    const botonGuardarCambios = document.getElementById("botonGuardarCambios");
-
-    //si hay una funcion en la memoria, la elimino
-    if (funcionParaBotonEditar !== null) {
-      botonGuardarCambios.removeEventListener("click", funcionParaBotonEditar);
-      //reseteo porque borre el evento
-      funcionParaBotonEditar = null;
-    }
-  });
-
-function mostrarEtiquetas(input, array) {
-  console.log(input);
-  for (let i = 0; i < array.length; i++) {
-    const contenedorEtiqueta = document.createElement("div");
-    const etiqueta = document.createElement("span");
-    etiqueta.textContent = array[i];
-    //<i class="bi bi-backspace-reverse"></i>
-    const icono = document.createElement("i");
-    icono.classList.add("bi", "bi-backspace-reverse");
-
-    icono.classList.add("btn", "btn-sm");
-    contenedorEtiqueta.classList.add(coloresRandon());
-    contenedorEtiqueta.classList.add("badge", "rounded-pill");
-    etiqueta.classList.add("choclo");
-
-    contenedorEtiqueta.appendChild(etiqueta);
-    etiqueta.appendChild(icono);
-    input.appendChild(contenedorEtiqueta);
-
-    icono.addEventListener("click", function (e) {
-      console.log("estoy en el boton");
-      //eliminar etiqueta
-      contenedorEtiqueta.classList.add("");
-    });
-  }
-}
-
-function coloresRandon() {
-  const colores = [
-    "text-bg-primary",
-    "text-bg-secondary",
-    "text-bg-success",
-    "text-bg-danger",
-    "text-bg-warning",
-    "text-bg-info",
-    "text-bg-light",
-    "text-bg-dark",
-    "text-bg-white",
-  ];
-  const randomIndex = Math.floor(Math.random() * colores.length);
-  return colores[randomIndex];
-}
-
-mostrarProductos();
+actualizarTablaCompleta()
--- modulos/crearDomElements.js
+++ modulos/crearDomElements.js
@@ -1,9 +1,10 @@
-import { saveCarritos } from "./carritos.js"
-import * as sesionActual from "./sesionActual.js"
-import * as stock from "./stock.js"
-import * as carritos from "./carritos.js"
+import { saveCarritos } from "./carritos.js";
+import * as sesionActual from "./sesionActual.js";
+import * as stock from "./stock.js";
+import * as carritos from "./carritos.js";
 
-{/* <div class="col d-flex justify-content-center">
+{
+  /* <div class="col d-flex justify-content-center">
     <div class="card w-100">
         <img src="img\medialuna.png" class="card-img-top" alt="medialuna">
         <div class="card-body d-flex flex-column">
@@ -15,9 +16,11 @@ import * as carritos from "./carritos.js"
             </div>
         </div>
     </div>
-</div> */}
+</div> */
+}
 
-{/* <div class="d-flex flex-wrap rounded rounded-2 border p-neutral mb-neutral">
+{
+  /* <div class="d-flex flex-wrap rounded rounded-2 border p-neutral mb-neutral">
     <div class="w-15">
         <img src="img/medialuna.png" alt="medialuna" class="w-100 rounded rounded-2">
     </div>
@@ -36,73 +39,166 @@ import * as carritos from "./carritos.js"
             <button type="button" class="btn btn-info"><i class="bi bi-trash3"></i></button>
         </div>
     </div>
-</div> */}
+</div> */
+}
 
 function handlerAñadirACarrito() {
-    carritos.getCarrito(sesionActual.get().nombreUsuario).setProducto(stock.getProducto(this.dataset.id))
-    saveCarritos()
+  carritos
+    .getCarrito(sesionActual.get().nombreUsuario)
+    .setProducto(stock.getProducto(this.dataset.id));
+  saveCarritos();
 }
 /**
  * Crea una tarjeta de un Producto dado para mostrarla en la tienda.
- * 
+ *
  * @param {Producto} producto - El objeto Producto sobre el cual se quiere crear la tarjeta.
  * @param {*} divAgregar
  */
 export function createTarjetaTienda(producto, divAgregar) {
-    // Primero
-    const divContenedor = document.createElement("div")
-    divContenedor.classList.add("col", "d-flex", "justify-content-center")
-    // Segundo
-    const divTarjeta = document.createElement("div")
-    divTarjeta.classList.add("card", "w-100")
-    // Tercero
-    const img = document.createElement("img")
-    img.src = producto.imagen
-    img.alt = producto.nombre
-    img.classList.add("card-img-top")
-    const divBody = document.createElement("div")
-    divBody.classList.add("card-body", "d-flex", "flex-column")
-    // Cuarto
-    const h5 = document.createElement("h5")
-    h5.textContent = producto.nombre
-    h5.classList.add("card-title")
-    const p = document.createElement("p")
-    p.textContent = producto.descripcion
-    p.classList.add("card-text")
-    const divPrecio = document.createElement("div")
-    divPrecio.classList.add("d-flex", "justify-content-between", "align-items-center")
-    // Quinto
-    const span = document.createElement("span")
-    span.textContent = `$${producto.valor} C/U`
-    span.classList.add("fs-5")
-    const button = document.createElement("button")
-    button.dataset.id = producto.id
-    button.classList.add("btn", "btn-primary", "align-self-end")
-    button.addEventListener("click", handlerAñadirACarrito)
-    // Sexto
-    const i = document.createElement("i")
-    i.classList.add("bi", "bi-cart-plus")
+  // Primero
+  const divContenedor = document.createElement("div");
+  divContenedor.classList.add("col", "d-flex", "justify-content-center");
+  // Segundo
+  const divTarjeta = document.createElement("div");
+  divTarjeta.classList.add("card", "w-100");
+  // Tercero
+  const img = document.createElement("img");
+  img.src = producto.imagen;
+  img.alt = producto.nombre;
+  img.classList.add("card-img-top");
+  const divBody = document.createElement("div");
+  divBody.classList.add("card-body", "d-flex", "flex-column");
+  // Cuarto
+  const h5 = document.createElement("h5");
+  h5.textContent = producto.nombre;
+  h5.classList.add("card-title");
+  const p = document.createElement("p");
+  p.textContent = producto.descripcion;
+  p.classList.add("card-text");
+  const divPrecio = document.createElement("div");
+  divPrecio.classList.add(
+    "d-flex",
+    "justify-content-between",
+    "align-items-center",
+  );
+  // Quinto
+  const span = document.createElement("span");
+  span.textContent = `$${producto.valor} C/U`;
+  span.classList.add("fs-5");
+  const button = document.createElement("button");
+  button.dataset.id = producto.id;
+  button.classList.add("btn", "btn-primary", "align-self-end");
+  button.addEventListener("click", handlerAñadirACarrito);
+  // Sexto
+  const i = document.createElement("i");
+  i.classList.add("bi", "bi-cart-plus");
 
-    // Anidamiento
-    button.append(i)
-    divPrecio.append(span, button)
-    divBody.append(h5, p, divPrecio)
-    divTarjeta.append(img, divBody)
-    divContenedor.append(divTarjeta)
-    divAgregar.append(divContenedor)
+  // Anidamiento
+  button.append(i);
+  divPrecio.append(span, button);
+  divBody.append(h5, p, divPrecio);
+  divTarjeta.append(img, divBody);
+  divContenedor.append(divTarjeta);
+  divAgregar.append(divContenedor);
 }
 
 /**
- * 
- * 
- * @param {Producto} producto 
+ *
+ *
+ * @param {Producto} producto
  * @returns {HTMLDivElement}
  */
 export function createTarjetaCarrito(producto) {
-    // Primero
-    const divContenedor = document.createElement("div")
-    divContenedor.classList.add("d-flex", "flex-wrap", "rounded rounded-2", "border", "p-neutral", "mb-neutral")
+  // Primero
+  const divContenedor = document.createElement("div");
+  divContenedor.classList.add(
+    "d-flex",
+    "flex-wrap",
+    "rounded rounded-2",
+    "border",
+    "p-neutral",
+    "mb-neutral",
+  );
+
+  return divContenedor;
+}
+
+export function crearFilaProducto(producto,indice,funcionEliminar,functionEditar,
+) {
+  const tr = document.createElement("tr");
+  tr.dataset.id = producto.id;
+
+  const tdNumero = document.createElement("td");
+  tdNumero.textContent = indice;
+
+  const tdTitulo = document.createElement("td");
+  tdTitulo.textContent = producto.nombre;
+
+  const tdInfo = document.createElement("td");
+  tdInfo.textContent = producto.descripcion;
+
+  const tdStock = document.createElement("td");
+  tdStock.textContent = producto.cantidad;
+
+  const tdEtiquetas = document.createElement("td");
+  if (producto.etiquetas && producto.etiquetas.length > 0) {
+    for (let i = 0; i < producto.etiquetas.length; i++) {
+      const etiqueta = producto.etiquetas[i];
+      const spanEtiqueta = document.createElement("span");
+      spanEtiqueta.classList.add("badge", "rounded-pill", "me-1", coloresRandom());
+      spanEtiqueta.textContent = etiqueta;
+      tdEtiquetas.appendChild(spanEtiqueta);
+    }
+  }
+
+  const tdValor = document.createElement("td");
+  tdValor.textContent = `$${producto.valor}`;
+
+  const tdAcciones = document.createElement("td");
+  const divContenedor = document.createElement("div");
+  divContenedor.classList.add("d-flex", "gap-3", "align-items-center");
+
+  const iconoBorrar = document.createElement("i");
+  iconoBorrar.classList.add("bi", "bi-trash3-fill", "text-danger");
+  iconoBorrar.style.cursor = "pointer";
+  iconoBorrar.addEventListener("click", function() {
+    funcionEliminar(producto.id)
+  });
+
+  const iconoEditar = document.createElement("i");
+  iconoEditar.classList.add("bi", "bi-pencil-square", "text-warning");
+  iconoEditar.style.cursor = "pointer";
+  iconoEditar.setAttribute("data-bs-toggle", "modal");
+  iconoEditar.setAttribute("data-bs-target", "#modalEditar");
+  iconoEditar.addEventListener("click", function(){
+    functionEditar(producto.id)
+  });
+
+  divContenedor.append(iconoBorrar, iconoEditar);
+  tdAcciones.appendChild(divContenedor);
+
+  // Unimos todas las celdas en la fila
+  tr.append(
+    tdNumero,
+    tdTitulo,
+    tdInfo,
+    tdStock,
+    tdEtiquetas,
+    tdValor,
+    tdAcciones,
+  );
+  return tr;
+}
 
 
-    return divContenedor
+function coloresRandom() {
+  const colores = [
+    "text-bg-primary",
+    "text-bg-success",
+    "text-bg-danger",
+    "text-bg-warning",
+    "text-bg-info",
+    "text-bg-dark",
+  ];
+  return colores[Math.floor(Math.random() * colores.length)];
 }
