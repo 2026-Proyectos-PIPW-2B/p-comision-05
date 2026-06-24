eb3c9713768e0708749dcebff5954d9e29507e58
Joaquin <joacopompilio@gmail.com>
2026-06-24 20:15:26 -0300

    Pull a master

--- js/login.js
+++ js/login.js
@@ -1,51 +1,48 @@
 import * as users from "../modulos/usuarios.js";
 
-const inputNombre = document.getElementById("inputNombre")
-const inputPassword = document.getElementById("inputPassword")
-const inputBotonLogin = document.getElementById("botonLogin")
-const inputMensajeNombre = document.getElementById("mensajeNombre")
-const inputMensajePassword = document.getElementById("mensajeContraseña")
+const inputNombre = document.getElementById("inputNombre");
+const inputPassword = document.getElementById("inputPassword");
+const inputBotonLogin = document.getElementById("botonLogin");
+const inputMensajeNombre = document.getElementById("mensajeNombre");
+const inputMensajePassword = document.getElementById("mensajeContraseña");
 
 function vaciarMensajes() {
-    inputNombre.classList.remove("is-invalid , is-valid")
-    inputMensajeNombre.textContent = ""
+  inputNombre.classList.remove("is-invalid", "is-valid");
+  inputMensajeNombre.textContent = "";
 
-    inputPassword.classList.remove("is-invalid, is-valid")
-    inputMensajePassword.textContent = ""
+  inputPassword.classList.remove("is-invalid", "is-valid");
+  inputMensajePassword.textContent = "";
 }
 
 inputBotonLogin.addEventListener("click", function (e) {
-    console.log("Entre a listener")
-    e.preventDefault()
-    let nombre = inputNombre.value
-    let password = inputPassword.value
-    let usuario = users.getUsuario(nombre)
+  e.preventDefault();
 
-    if (usuario === null) {
-        vaciarMensajes()
-        inputNombre.classList.add("is-invalid")
-        inputMensajeNombre.textContent = "Usuario Incorrecto"
+  let nombre = inputNombre.value;
+  let password = inputPassword.value;
+  let usuario = users.getUsuario(nombre);
+  let nombreUsuario = usuario.nombreUsuario;
+  console.log(nombreUsuario)
 
+  if (usuario === null) {
+    vaciarMensajes();
+    inputNombre.classList.add("is-invalid");
+    inputMensajeNombre.textContent = "Usuario Incorrecto";
+  } else {
+    if (usuario.contraseña != password) {
+      vaciarMensajes();
+      inputNombre.classList.add("is-valid");
+      inputMensajeNombre.textContent = "Usuario Correcto";
+
+      inputPassword.classList.add("is-invalid");
+      inputMensajePassword.textContent = "Contraseña Incorrecto";
+      console.log("estoy aqui");
     } else {
-        if (usuario.contraseña != password) {
-            vaciarMensajes()
-            inputNombre.classList.add("is-valid")
-            inputMensajeNombre.textContent = "Usuario Correcto"
-
-            inputPassword.classList.add("is-invalid")
-            inputMensajePassword.textContent = "Contraseña Incorrecto"
-             console.log("estoy aqui")
-
-        } else {
-            vaciarMensajes()
-            if (usuario.tipo === "admin") {
-                window.location.href = "control-de-productos.html";
-            } else {
-                window.location.href = "tienda.html"
-            }
-        }
+      vaciarMensajes();
+      if (usuario.tipo === "admin") {
+        window.location.href = "control-de-productos.html";
+      } else {
+        window.location.href = "tienda.html";
+      }
     }
-})
-
-
-
+  }
+});
