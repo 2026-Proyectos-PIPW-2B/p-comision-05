20c56cd567c83fbf0f2ab74b5016d47b0d1dc7f8
Joaquin <joacopompilio@gmail.com>
2026-06-29 20:47:27 -0300

    Se termino de crear productosDiponibles

--- modulos/productosDisponibles.js
+++ modulos/productosDisponibles.js
@@ -2,10 +2,28 @@
 const productosDisponibles = [
     "cara-sucia",
     "medialuna-dulce",
-    ""
+    "cañoncito",
+    "caprese",
+    "chipa",
+    "churros",
+    "dona",
+    "matera-de-chocolate",
+    "matera-de-manzana",
+    "matera-de-naranja",
+    "medialuna-salada",
+    "morron-y-aceitunas",
+    "muzzarella-y-tomate",
+    "palito-chino",
+    "palmerita",
+    "pasta-frola",
+    "sacramento",
+    "torta-80-golpes",
+    "tortafrita",
+    "vigilante",
 ]
 
 
 export function get() {
-    return
+    return productosDisponibles;
 }
+
