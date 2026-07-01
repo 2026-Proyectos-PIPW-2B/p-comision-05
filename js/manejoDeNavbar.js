39133036f9a6860bfc5216a94deb6690ee42aec1
FacundoVarela <facundovarelagit@gmail.com>
2026-07-01 19:11:42 -0300

    cambio de login a index

--- js/manejoDeNavbar.js
+++ js/manejoDeNavbar.js
@@ -5,18 +5,23 @@ const buttonCerrarSesion = document.getElementById("buttonCerrarSesion")
 
 function cerrarSesion() {
     sesionActual.clear()
-    window.location.href = "login.html"
+    window.location.href = "index.html"
 }
 
 function onLoad() {
     const usuarioLogueado = sesionActual.get();
+    if (usuarioLogueado == null) {
+        window.location.href = "index.html"
+    }
+    else {
+        const nombreMayuscula = usuarioLogueado.nombre.charAt(0).toUpperCase() + usuarioLogueado.nombre.slice(1)
+        const apellidoMayuscula = usuarioLogueado.apellido.charAt(0).toUpperCase() + usuarioLogueado.apellido.slice(1)
+        textoSesion.textContent = `${nombreMayuscula} ${apellidoMayuscula}`
     
-    const nombreMayuscula = usuarioLogueado.nombre.charAt(0).toUpperCase() + usuarioLogueado.nombre.slice(1)
-    const apellidoMayuscula = usuarioLogueado.apellido.charAt(0).toUpperCase() + usuarioLogueado.apellido.slice(1)
-    textoSesion.textContent = `${nombreMayuscula} ${apellidoMayuscula}`
+        //todas los html, que linkeen este js, tienen que tener un boton con id: "buttonCerrarSesion" 
+        buttonCerrarSesion.addEventListener("click", cerrarSesion)
 
-    //todas los html, que linkeen este js, tienen que tener un boton con id: "buttonCerrarSesion" 
-    buttonCerrarSesion.addEventListener("click", cerrarSesion)
+    }
 }
 
 window.addEventListener("load", onLoad)
